3e424516f3f456d5ef93ad61bbdf4bd1663e8907
anatoli-derese <135621823+anatoli-derese@users.noreply.github.com>
2024-03-05 16:46:15 +0300

    Add files via upload

---
+++ LAB-1-Functions.dart
@@ -0,0 +1,12 @@
+void main(){
+  List<int> nums= [1,2,3,4,5];
+  print(average(nums));
+}
+double average(List<int> nums){
+  int accumulator = 0;
+  for (int num in nums){
+    accumulator += num;
+  }
+  double average = accumulator / nums.length;
+  return average;
+}
