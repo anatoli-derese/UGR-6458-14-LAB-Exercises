c796509f217d5b845001a9ee1ca6e679fa719d99
anatoli-derese <anatoli.derese@aait.edu.et>
2024-05-21 15:20:15 +0300

    Kotlin Excercises

--- kotlin_excercise/class.kt
+++ kotlin_excercise/class.kt
@@ -16,7 +16,43 @@ class BankAccount(private val accountNumber: String, private var balance: Double
         println("Account $accountNumber balance: \$$balance")
     }
 }
+# Compiled class file
+*.class
 
+# Log file
+*.log
+
+# Package Files #
+*.jar
+*.war
+*.nar
+*.ear
+*.zip
+*.tar.gz
+*.rar
+
+# Kotlin Build
+*.kotlin_metadata
+*.kotlin_builtins
+*.kotlin_module
+
+# Gradle files
+.gradle/
+build/
+
+# IntelliJ IDEA files
+.idea/
+*.iml
+*.iws
+
+# VS Code files
+.vscode/
+
+# Other
+*.bak
+*.swp
+*~
+*.DS_Store
 fun main() {
     val account = BankAccount("123456", 1000.0)
     account.deposit(500.0)
